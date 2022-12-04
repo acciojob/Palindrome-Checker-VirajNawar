0d4785ac3eb9f6f18762301d61ff51ddb9b5e832
Viraj <virajnawar4063@gmail.com>
2022-12-04 21:33:00 +0530

    solution

--- solution.js
+++ solution.js
@@ -1,6 +1,17 @@
 // complete the given function
 
 function palindrome(str){
+let start = 0
+let end = str.length - 1
+let result  = true 
+
+while (start < end) {
+    if (str[start].toLowerCase() !== start[end].toLowerCase()){
+        result = false
+    }
+    start++
+    end--
+}
 
 }
 module.exports = palindrome
